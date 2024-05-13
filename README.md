4cac48e875b27363441e95390ab6f0f6db40de7b
ddalkyTokky <tnsdyd6933@naver.com>
2024-05-13 14:58:10 +0900

    first setting

---
+++ README.md
@@ -0,0 +1,5 @@
+# 1. USE CASE
+# 2. ERD
+![todo_list](https://github.com/ddalkyTokky/KotlinSpring_TODO_Backend/assets/47583083/4a768646-6a63-490b-8d16-6fd3d59649f7)
+
+# 3. API
---
+++ src/test/kotlin/com/soonyong/todo/TodoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.soonyong.todo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TodoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
