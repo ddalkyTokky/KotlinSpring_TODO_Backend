a91ed1e301e857598f3bbcea7c72b1b5aebc289d
ddalkyTokky <tnsdyd6933@naver.com>
2024-05-16 11:14:58 +0900

    build.gradle.kts 에 validation, test환경에서의 lombok 추가.

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,10 +27,17 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.0.2")
+
 	compileOnly("org.projectlombok:lombok")
 	runtimeOnly("com.mysql:mysql-connector-j")
 	annotationProcessor("org.projectlombok:lombok")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+	testCompileOnly("org.projectlombok:lombok")
+	testAnnotationProcessor("org.projectlombok:lombok")
 }
 
 tasks.withType<KotlinCompile> {
