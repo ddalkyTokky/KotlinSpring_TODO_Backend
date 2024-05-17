dd393c4000398100c1d64bec679573f77155d9c2
ddalkyTokky <tnsdyd6933@naver.com>
2024-05-17 10:39:37 +0900

    MemberRepository createMember 테스트 코드 추가

---
+++ src/test/kotlin/com/soonyong/todo/domain/member/repository/MemberRepositoryTest.kt
@@ -0,0 +1,27 @@
+package com.soonyong.todo.domain.member.repository
+
+import com.soonyong.todo.domain.member.model.Member
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.annotation.Rollback
+import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.transaction.annotation.Transactional
+
+@Transactional
+@SpringBootTest
+class MemberRepositoryTest () {
+    @Autowired
+    private lateinit var memberRepository : MemberRepository
+
+    @Test
+    @Rollback(false)
+    fun memberCreateTest() {
+        memberRepository.save(
+            Member.createMember(
+                "이순용",
+                "helloworld"
+            )
+        )
+    }
+}
