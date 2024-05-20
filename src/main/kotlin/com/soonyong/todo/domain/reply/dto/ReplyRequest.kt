f7eafb1d0ad7abf380dfd119be0ce82677f0e87b
ddalkyTokky <tnsdyd6933@naver.com>
2024-05-20 14:38:10 +0900

    TODO 추가

--- src/main/kotlin/com/soonyong/todo/domain/reply/dto/ReplyRequest.kt
+++ src/main/kotlin/com/soonyong/todo/domain/reply/dto/ReplyRequest.kt
@@ -1,6 +1,6 @@
 package com.soonyong.todo.domain.reply.dto
 
 data class ReplyRequest (
-    val member_id: Long,
+    val member_id: Long, // TODO 로그인 기능 도입 후 삭제 예정
     val content: String?
 )
--- src/main/kotlin/com/soonyong/todo/domain/todo/dto/TodoCreateRequest.kt
+++ src/main/kotlin/com/soonyong/todo/domain/todo/dto/TodoCreateRequest.kt
@@ -3,7 +3,7 @@ package com.soonyong.todo.domain.todo.dto
 import com.soonyong.todo.domain.todo.model.TodoStatus
 
 data class TodoCreateRequest (
-    val member_id: Long,
+    val member_id: Long, // TODO 로그인 기능 도입 후 삭제 예정
     val title: String,
     val content: String
 )
