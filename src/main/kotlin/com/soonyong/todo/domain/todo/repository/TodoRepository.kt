719e0b7f8385a4ae30c8e7f165714f63da4ed55e
ddalkyTokky <tnsdyd6933@naver.com>
2024-05-21 18:00:48 +0900

    Step4 첫번째 요구조건에 따른 N + 1 스터디 중단. Todo List를 불러올때 댓글을 불러오는 것은 상당히 억지스러운 요구사항임. 다만, 확인 결과, N + 1 문제가 발생하는 것과, fetch Join 을 활용한다는 것까지 확인함.

--- src/main/kotlin/com/soonyong/todo/domain/todo/repository/TodoRepository.kt
+++ src/main/kotlin/com/soonyong/todo/domain/todo/repository/TodoRepository.kt
@@ -2,6 +2,7 @@ package com.soonyong.todo.domain.todo.repository
 
 import com.soonyong.todo.domain.todo.model.Todo
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 
 interface TodoRepository: JpaRepository<Todo, Long> {
     fun findAllByOrderByCreatedAtAsc(): List<Todo>
@@ -9,4 +10,7 @@ interface TodoRepository: JpaRepository<Todo, Long> {
 
     fun findAllByMemberNameOrderByCreatedAtAsc(name: String?): List<Todo>
     fun findAllByMemberNameOrderByCreatedAtDesc(name: String?): List<Todo>
+
+//    @Query("SELECT t FROM Todo t join fetch t.replies")
+//    fun findAllWithRepliesUsingFetchJoin(): List<Todo>
 }
