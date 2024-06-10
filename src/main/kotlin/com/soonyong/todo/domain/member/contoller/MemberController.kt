2c2989da21c5fba9d172b193de3e1497424afc0c
ddalkyTokky <tnsdyd6933@naver.com>
2024-06-10 12:26:47 +0900

    Feat: memberController 에 일단 로직 우겨넣는중.

--- src/main/kotlin/com/soonyong/todo/domain/member/contoller/MemberController.kt
+++ src/main/kotlin/com/soonyong/todo/domain/member/contoller/MemberController.kt
@@ -5,14 +5,21 @@ import com.soonyong.todo.domain.member.dto.MemberResponse
 import com.soonyong.todo.domain.member.service.MemberService
 import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
+import org.springframework.http.HttpStatusCode
+import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
+import org.springframework.util.LinkedMultiValueMap
 import org.springframework.web.bind.annotation.*
+import org.springframework.web.client.RestClient
+import org.springframework.web.client.body
 
 @RequestMapping("member")
 @RestController
 class MemberController (
     private val memberService: MemberService
 ){
+    private val restClient: RestClient = RestClient.create()
+
     @PostMapping("new")
     fun createMember(@RequestBody @Valid memberRequest: MemberRequest): ResponseEntity<MemberResponse>{
         return ResponseEntity
@@ -28,9 +35,21 @@ class MemberController (
     }
 
     @GetMapping("redirect")
-    fun redirect(@RequestParam requestParam: Map<String, String>): ResponseEntity<String>{
-        return ResponseEntity
-            .status(HttpStatus.FOUND)
-            .body(requestParam.get("code"))
+    fun redirect(@RequestParam requestParam: Map<String, String>): String{
+        val requestData = mutableMapOf(
+            "grant_type" to "authorization_code",
+            "client_id" to "SoonYong",
+            "code" to requestParam.get("code")
+        )
+        return restClient.post()
+            .uri("https://kauth.kakao.com/oauth/token")
+            .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+            .body(LinkedMultiValueMap<String, String>().apply { this.setAll(requestData) })
+            .retrieve()
+            .onStatus(HttpStatusCode::isError) { _, _ ->
+                throw RuntimeException("카카오 AccessToken 조회 실패")
+            }
+            .body<String>()
+            ?: throw RuntimeException("카카오 AccessToken 조회 실패")
     }
 }
